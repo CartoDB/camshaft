62e2351559f5519bf5e9a0f9a41239ad19625e90
Raul Ochoa <rochoaf@gmail.com>
2016-03-09 15:38:13 +0100

    Node creator to create Node classes with param validation

---
+++ test/unit/node-creation.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var assert = require('assert');
+
+var Node = require('../../lib/node/node');
+
+describe('node-creation', function() {
+
+    var TestSource = Node.create('test-source', {query: Node.PARAM_TYPE.TEXT});
+
+    it('should validate params', function() {
+        var QUERY = 'select * from table';
+        var source = new TestSource({ query: QUERY });
+        assert.equal(source.query, QUERY);
+    });
+
+    it('should fail for missing params', function() {
+        var source;
+        assert.throws(
+            function() {
+                 source = new TestSource({});
+            },
+            function(err) {
+                assert.equal(err.message, 'Missing required param "query"');
+                return true;
+            }
+        );
+    });
+
+    it('should fail for invalid param type', function() {
+        var source;
+        assert.throws(
+            function() {
+                source = new TestSource({ query: 2 });
+            },
+            function(err) {
+                assert.equal(err.message, 'Invalid type for param "query", expects a "TEXT" type, got `2`');
+                return true;
+            }
+        );
+    });
+
+});
