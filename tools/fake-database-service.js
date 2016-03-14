dfbbf327359f4abbf75743d0c45a003e5c1a9208
Raul Ochoa <rochoaf@gmail.com>
2016-03-14 11:03:48 +0100

    Allow to instantiate analysis with custom database service

---
+++ tools/fake-database-service.js
@@ -0,0 +1,37 @@
+'use strict';
+
+function FakeDatabaseService() {
+
+}
+
+module.exports = FakeDatabaseService;
+
+FakeDatabaseService.prototype = {
+    run: function(query, callback) {
+        return callback(null, {});
+    },
+
+    getAffectedTables: function(sql, callback) {
+        return callback(null, []);
+    },
+
+    createTable: function(targetTableName, query, callback) {
+        return callback(null, {});
+    },
+
+    getColumnNames: function(query, callback) {
+        return callback(null, []);
+    },
+
+    enqueue: function(query, callback) {
+        return callback(null, {status: 'ok'});
+    },
+
+    registerNodesInCatalog: function(nodes, callback) {
+        return callback(null);
+    },
+
+    trackNode: function(node, callback) {
+        return callback(null);
+    }
+};
