960acd5ab4b3e02c9ef7bf5184dcc990d837457c
Raul Ochoa <rochoaf@gmail.com>
2016-04-06 14:13:15 +0200

    Bump version and add new reference

--- reference/versions/index.js
+++ reference/versions/index.js
@@ -1,7 +1,8 @@
 var versions = {
     "0.1.0": require("./0.1.0/reference.json"),
-    "0.2.0": require("./0.2.0/reference.json")
+    "0.2.0": require("./0.2.0/reference.json"),
+    "0.3.0": require("./0.3.0/reference.json")
 };
 
-versions.latest = versions["0.2.0"];
+versions.latest = versions["0.3.0"];
 module.exports = versions;
