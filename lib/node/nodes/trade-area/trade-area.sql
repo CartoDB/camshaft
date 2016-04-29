edb390f1ecb9b321b5d17d24c2cd37f892f8bf3d
Daniel García Aubert <dgaubert@cartodb.com>
2016-04-29 13:01:24 +0200

    Refactor in order to improve readbility in analysis queries

---
+++ lib/node/nodes/trade-area/trade-area.sql
@@ -0,0 +1,20 @@
+WITH
+_cdb_analysis_source_points AS (
+  {{=it.pointsQuery}}
+),
+_cdb_analysis_isochrones AS (
+  SELECT
+    cdb_isochrone(
+      _cdb_analysis_source_points.the_geom,
+      '{{=it.kind}}',
+      ARRAY[{{=it.isolines}}]::integer[]
+    ) as isochrone,
+    {{=it.columnsQuery}}
+  FROM _cdb_analysis_source_points
+)
+SELECT
+  (isochrone).center,
+  (isochrone).data_range,
+  (isochrone).the_geom,
+  {{=it.columnsQuery}}
+FROM _cdb_analysis_isochrones
