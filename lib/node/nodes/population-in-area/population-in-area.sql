e66ebccdfeb071a2431398e6151e82476e5538f5
Daniel García Aubert <dgaubert@cartodb.com>
2016-04-29 16:11:17 +0200

    Added tests for buffer query builder and population in area query builder

--- lib/node/nodes/population-in-area/population-in-area.sql
+++ lib/node/nodes/population-in-area/population-in-area.sql
@@ -4,10 +4,3 @@ SELECT
 FROM (
   {{=it.source}}
 ) _camshaft_population_in_area
-
--- CDB_Population fake implementation
--- ---------------------------------------------------------------------------
--- CREATE FUNCTION CDB_Population(geometry) RETURNS numeric AS
--- 'select (random() * 1e6)::numeric;'
--- LANGUAGE SQL VOLATILE;
--- ---------------------------------------------------------------------------
