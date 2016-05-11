f7cc28caa02e3ce80499daf8b0790e0f24211be5
Raul Ochoa <rochoaf@gmail.com>
2016-05-11 16:12:36 +0200

    Reference for 0.7.0 version

--- reference/versions/index.js
+++ reference/versions/index.js
@@ -3,8 +3,9 @@ var versions = {
     "0.2.0": require("./0.2.0/reference.json"),
     "0.3.0": require("./0.3.0/reference.json"),
     "0.4.0": require("./0.4.0/reference.json"),
-    "0.6.0": require("./0.6.0/reference.json")
+    "0.6.0": require("./0.6.0/reference.json"),
+    "0.7.0": require("./0.7.0/reference.json")
 };
 
-versions.latest = versions["0.6.0"];
+versions.latest = versions["0.7.0"];
 module.exports = versions;
