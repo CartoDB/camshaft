11cba8824235959661201bfbdfe93a63857aa987
Raul Ochoa <rochoaf@gmail.com>
2016-05-11 11:10:56 +0200

    Allow to have optional/nullable params
    You need to define a param like Node.PARAM.NULLABLE(Node.PARAM.STRING)

--- test/unit/node-creation.js
+++ test/unit/node-creation.js
@@ -69,4 +69,62 @@ describe('node-creation', function() {
 
     });
 
+    describe('Node.PARAM.NULLABLE', function() {
+        var NullableNode = Node.create('test-nullable', {
+            mandatory: Node.PARAM.STRING,
+            optional: Node.PARAM.NULLABLE(Node.PARAM.STRING)
+        });
+
+        it('should work for valid params', function() {
+            var nullableNode = new NullableNode({ mandatory: 'wadus_mandatory', optional: 'wadus_optional' });
+            assert.equal(nullableNode.mandatory, 'wadus_mandatory');
+            assert.equal(nullableNode.optional, 'wadus_optional');
+        });
+
+        it('should fail for invalid PARAM type', function() {
+            var nullableNode;
+            assert.throws(
+                function() {
+                    nullableNode = new NullableNode({ mandatory: 'wadus_mandatory', optional: 1 });
+                },
+                function(err) {
+                    assert.equal(
+                        err.message,
+                        'Invalid type for param "optional", expects "string" type, got `1`'
+                    );
+                    return true;
+                }
+            );
+        });
+
+        it('should fail for invalid PARAM type with falsy values', function() {
+            var nullableNode;
+            assert.throws(
+                function() {
+                    nullableNode = new NullableNode({ mandatory: 'wadus_mandatory', optional: 0 });
+                },
+                function(err) {
+                    assert.equal(
+                        err.message,
+                        'Invalid type for param "optional", expects "string" type, got `0`'
+                    );
+                    return true;
+                }
+            );
+        });
+
+        it('should allow to use null values for NULLABLE params', function() {
+            var nullableNode = new NullableNode({ mandatory: 'wadus_mandatory', optional: null });
+            assert.equal(nullableNode.mandatory, 'wadus_mandatory');
+            assert.equal(nullableNode.optional, null);
+        });
+
+        it('should allow to use undefined values for NULLABLE params', function() {
+            var nullableNode = new NullableNode({ mandatory: 'wadus_mandatory' });
+            assert.equal(nullableNode.mandatory, 'wadus_mandatory');
+            assert.equal(nullableNode.optional, null);
+        });
+
+    });
+
 });
