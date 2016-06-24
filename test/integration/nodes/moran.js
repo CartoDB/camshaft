de2b5dceac94ffc863acaa8a74134e543e318ca3
Raul Ochoa <rochoaf@gmail.com>
2016-06-24 15:21:35 +0200

    Automatically populate nodes from nodes directory
    This removes the extra work of adding nodes manually to that list.

--- test/integration/nodes/moran.js
+++ test/integration/nodes/moran.js
@@ -1,15 +1,16 @@
 'use strict';
 
 var assert = require('assert');
-var nodes = require('../../../lib/node');
+var Source = require('../../../lib/node/nodes/source');
+var Moran = require('../../../lib/node/nodes/moran');
 
 describe('moran', function() {
 
     var column = 'wadus_column';
-    var source = new nodes.Source({query: 'select * from table'});
+    var source = new Source({query: 'select * from table'});
 
     it('should use knn as default w_type param', function() {
-        var moran = new nodes.Moran({
+        var moran = new Moran({
             source: source,
             numerator_column: column,
             significance: 0.05
