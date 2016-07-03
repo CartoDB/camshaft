511ccd3e59690fb16cd1e6b9e7c958f02001f2ce
javi <qualopec@gmail.com>
2016-07-03 18:14:09 +0200

    linter

--- test/integration/nodes/spatial-markov-trend.js
+++ test/integration/nodes/spatial-markov-trend.js
@@ -19,7 +19,8 @@ describe('spatial-markov-trend', function() {
         '  m.trend_up,',
         '  m.trend_down,',
         '  m.volatility',
-        'FROM CDB_SpatialMarkovTrend(\'select * from table\', Array[\'year0\',\'year1\'], 5, \'knn\', 5, 2, \'the_geom\', \'cartodb_id\') As m',
+        'FROM CDB_SpatialMarkovTrend(\'select * from table\', Array[\'year0\',\'year1\'], \
+5, \'knn\', 5, 2, \'the_geom\', \'cartodb_id\') As m',
         'JOIN (select * from table) input_query',
         'ON input_query.cartodb_id = m.rowid;\n'
         ].join('\n'));
@@ -35,7 +36,8 @@ describe('spatial-markov-trend', function() {
         '  m.trend_up,',
         '  m.trend_down,',
         '  m.volatility',
-        'FROM CDB_SpatialMarkovTrend(\'select * from table\', Array[\'year0\',\'year1\'], 5, \'queen\', 5, 21, \'geom\', \'cartodb_id\') As m',
+        'FROM CDB_SpatialMarkovTrend(\'select * from table\', Array[\'year0\',\'year1\'], \
+5, \'queen\', 5, 21, \'geom\', \'cartodb_id\') As m',
         'JOIN (select * from table) input_query',
         'ON input_query.cartodb_id = m.rowid;\n'
         ].join('\n'));
