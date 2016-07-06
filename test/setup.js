b57d25fbe15f7126a9f28206dbff53faa28a8c47
Raul Ochoa <rochoaf@gmail.com>
2016-07-06 16:32:50 +0200

    Create the schema before creating functions

--- test/setup.js
+++ test/setup.js
@@ -14,7 +14,6 @@ var DATABASE_NAME = testConfig.db.dbname;
 
 before(function setupTestDatabase(done) {
     var fixturePaths = [
-        fs.realpathSync('./test/fixtures/cdb_dataservices_client.sql'),
         fs.realpathSync('./test/fixtures/cdb_querytables_updated_at.sql'),
         fs.realpathSync('./test/fixtures/cdb_analysis_catalog.sql'),
         fs.realpathSync('./test/fixtures/cdb_isochrone.sql'),
@@ -37,6 +36,12 @@ before(function setupTestDatabase(done) {
             },
             function createPostgisExtension(stdout, stderr, callback) {
                 exec('psql -d ' + DATABASE_NAME + ' -c "CREATE EXTENSION postgis;"', callback);
+            },
+            function createSchema(stdout, stderr, callback) {
+                exec(
+                    'psql -d ' + DATABASE_NAME + ' -c "CREATE SCHEMA IF NOT EXISTS cdb_dataservices_client;"',
+                    callback
+                );
             },
             function applyFixtures(stdout, stderr, callback) {
                 async.map(fixturePaths, function (path, callback) {
