ff20a12628742ffed7f120d176492072f9158273
Raul Ochoa <rochoaf@gmail.com>
2016-08-12 18:07:16 +0200

    Split last updated at from input from isOutdated

---
+++ test/unit/node-updated-at.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var assert = require('assert');
+
+var Node = require('../../lib/node/node');
+
+describe('node-updated-at', function() {
+
+    function assertEqualTime(d1, d2) {
+        if (d1 === null) {
+            throw new Error('First argument must be a Date');
+        }
+
+        if (d2 === null) {
+            throw new Error('Second argument must be a Date');
+        }
+
+        assert.equal(d1.getTime(), d2.getTime());
+    }
+
+    var owner = 'localhost';
+    var START_TIME = new Date('1970-01-01T00:00:00.000Z');
+
+    var FooSourceNode = Node.create('foo-source', { buster: Node.PARAM.STRING() }, {
+        beforeCreate: function(node) {
+            node.setUpdatedAt(START_TIME);
+        }
+    });
+
+    var ChildNode = Node.create('child', {
+        source: Node.PARAM.NODE()
+    });
+
+
+    it('should retrieve parent node updated at with getLastUpdatedAtFromInputNodes', function() {
+        var aFooSource = new FooSourceNode(owner, { buster: 'a' });
+        var childNode = new ChildNode(owner, { source: aFooSource });
+        assert.equal(childNode.getUpdatedAt(), null);
+        assertEqualTime(aFooSource.getUpdatedAt(), childNode.getLastUpdatedAtFromInputNodes());
+    });
+});
