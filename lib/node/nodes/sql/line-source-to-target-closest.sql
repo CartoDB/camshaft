bcde92ac5c8bb6479b75d23dceb8989106c68343
Daniel García Aubert <dgaubert@cartodb.com>
2016-09-05 11:00:23 +0200

    Passed alias to the template

--- lib/node/nodes/sql/line-source-to-target-closest.sql
+++ lib/node/nodes/sql/line-source-to-target-closest.sql
@@ -3,10 +3,10 @@ SELECT
   ST_Length(the_geom) as length
 FROM (
   SELECT
-    target.cartodb_id as closest_id,
+    {{=it.target_alias}}.cartodb_id as closest_id,
     ST_MakeLine(
-      source.the_geom,
-      target.the_geom
+      {{=it.source_alias}}.the_geom,
+      {{=it.target_alias}}.the_geom
     ) AS the_geom,
     {{=it.source_columns}}
   FROM (
@@ -15,7 +15,7 @@ FROM (
     FROM (
       {{=it.source}}
     ) _source
-  ) source
+  ) {{=it.source_alias}}
   CROSS JOIN LATERAL (
     SELECT
       *
@@ -23,11 +23,11 @@ FROM (
       {{=it.target}}
     ) _target
     ORDER BY
-      source.the_geom <-> the_geom
+      {{=it.source_alias}}.the_geom <-> the_geom
     LIMIT 1
-  ) target
+  ) {{=it.target_alias}}
   {{? it.source_column && it.target_column}}
   WHERE
-    source.{{=it.source_column}} = target.{{=it.target_column}}
+    {{=it.source_alias}}.{{=it.source_column}} = {{=it.target_alias}}.{{=it.target_column}}
   {{?}}
 ) _cdb_analysis_lines
