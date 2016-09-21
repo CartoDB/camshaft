fadb110b56c59d920a9aee785e9e19a69ca717db
abelvm <abel@cartodb.com>
2016-09-21 14:43:58 +0200

    fitst commit

---
+++ test/acceptance/enrich.js
@@ -0,0 +1,57 @@
+// reescribir, que esto es una copia de cdb_contour sin editar
+
+'use strict';
+
+var assert = require('assert');
+var testHelper = require('../helper');
+
+
+describe('enrich', function () {
+
+    var QUERY_AIRBNB_ROOMS = 'select * from airbnb_rooms';
+
+    var QUERY_MADRID_DISTRICTS = 'select * from madrid_districts';
+
+    var analysisDefinition = {
+        type: 'enrich
+        ',
+        params: {
+            source: {
+                type: 'source',
+                params: {
+                    query: QUERY_AIRBNB_ROOMS
+                }
+            },
+            target: {
+                type: 'source',
+                params: {
+                    query: QUERY_MADRID_DISTRICTS
+                }
+            },
+            column: 'price',
+            method: 'barymetric',
+            number_of_neighbors: 0,
+            decay_order: 0
+        }
+    };
+
+    it('enrich by layer test', function (done) {
+        testHelper.createAnalyses(analysisDefinition, function (err, enrich) {
+
+            assert.ifError(err);
+
+            var rootNode = enrich.getRoot();
+
+            testHelper.getRows(rootNode.getQuery(), function (err, rows) {
+                assert.ifError(err);
+                rows.forEach(function (row) {
+                    assert.ok(typeof row.cartodb_id === 'number');
+                    assert.ok(typeof row.the_geom === 'string');
+                    assert.ok(typeof row.price === 'number');
+                });
+                return done();
+            });
+        });
+    });
+
+});
---
+++ test/fixtures/cdb_crankshaft/cdb_spatialinterpolation.sql
@@ -0,0 +1,17 @@
+-- reescribir, que esto es una copia de cdb_contour sin editar
+
+CREATE SCHEMA IF NOT EXISTS cdb_crankshaft;
+
+CREATE OR REPLACE FUNCTION cdb_crankshaft.CDB_SpatialInterpolation(
+    IN geomin geometry[],
+    IN colin numeric[],
+    IN point geometry,
+    IN method integer DEFAULT 1,
+    IN p1 numeric DEFAULT 0,
+    IN p2 numeric DEFAULT 0
+    )
+RETURNS numeric AS
+BEGIN
+    RETURN (p1 * p2 * colin[1]);
+END;
+$$ language plpgsql;
