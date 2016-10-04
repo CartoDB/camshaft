ec84c6cb8adf84bc9551d6990e41e0110f9cc2f5
Raul Ochoa <rochoaf@gmail.com>
2016-10-04 11:01:45 +0200

    Add basic test to validate source id does not change over time

---
+++ test/integration/nodes/source-id.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var assert = require('assert');
+var Source = require('../../../lib/node/nodes/source');
+
+describe('source-id', function() {
+    var owner = 'localhost';
+    it('source id change is unexpected', function() {
+        var source = new Source(owner, {query: 'select * from table'});
+        assert.equal(source.id(), '01ca52606f0aeb0de80639bb19db35710910456c');
+    });
+});
