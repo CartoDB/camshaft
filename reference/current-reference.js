fece163ec1cd8ca8a3988bafb44e234c1e57c599
Raul Ochoa <rochoaf@gmail.com>
2016-10-04 11:05:20 +0200

    Be able to generate reference with node options: version, cache, lazy

--- reference/current-reference.js
+++ reference/current-reference.js
@@ -3,10 +3,16 @@
 var version = require('../package.json').version;
 var nodes = require('../lib/node');
 
+var useOptions = !!process.env.OPTIONS;
+
 var analyses = Object.keys(nodes).reduce(function(analyses, modelName) {
     analyses[nodes[modelName].TYPE] = {
         params: nodes[modelName].PARAMS
     };
+
+    if (useOptions) {
+        analyses[nodes[modelName].TYPE].options = nodes[modelName].OPTIONS
+    }
 
     return analyses;
 }, {});
