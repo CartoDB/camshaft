2b301d657b2fdeae56a95e1f942765030b7d5b2b
Javier Goizueta <jgoizueta@gmail.com>
2016-10-11 15:50:25 +0200

    Fix the tests

--- test/setup.js
+++ test/setup.js
@@ -17,6 +17,7 @@ before(function setupTestDatabase(done) {
         fs.realpathSync('./test/fixtures/postgis_extension.sql'),
         fs.realpathSync('./test/fixtures/cdb_querytables_updated_at.sql'),
         fs.realpathSync('./test/fixtures/cdb_analysis_catalog.sql'),
+        fs.realpathSync('./test/fixtures/cdb_analysischeck.sql'),
 
         fs.realpathSync('./test/fixtures/cdb_dataservices_client/schema.sql'),
         fs.realpathSync('./test/fixtures/cdb_dataservices_client/cdb_geocoder.sql'),
