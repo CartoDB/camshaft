a88316eab0b55b946b6e38e5071d503cb7619392
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-04-11 16:49:22 +0200

    Going red: add test to fail if last_update is an invalid date

---
+++ test/unit/service/database.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var assert = require('assert');
+var Source = require('../../../lib/node/nodes/source');
+var DatabaseService = require('../../../lib/service/database');
+var testConfig = require('../../test-config');
+
+function isValidDate (d) {
+    return d instanceof Date && !isNaN(d.valueOf());
+}
+
+describe('database service', function () {
+    var SKIP = true;
+    var QUERY_NO_QUERYTABLES = 'select * from nulltime';
+
+    describe('.getMetadataFromAffectedTables()', function () {
+        beforeEach(function () {
+            this.databaseService = new DatabaseService(testConfig.user, testConfig.db, testConfig.batch);
+        });
+
+        // See: https://github.com/CartoDB/camshaft/issues/279
+        it('should not create invalid date for last_update', function (done) {
+            var originalQueryRunner = this.databaseService.queryRunner;
+            var runQueryStubCalled = false;
+            this.databaseService.queryRunner.run = function (sql, readOnly, callback) {
+                runQueryStubCalled = true;
+                callback(null, { rows: [] });
+            };
+            var source = new Source(testConfig.user, { query: QUERY_NO_QUERYTABLES });
+
+            this.databaseService.getMetadataFromAffectedTables(source, !SKIP, function (err, metadatadata) {
+                assert.ifError(err);
+
+                assert.ok(runQueryStubCalled);
+                assert.ok(metadatadata.hasOwnProperty('last_update'));
+                assert.ok(isValidDate(metadatadata.last_update), 'last_update is an invalid date');
+                this.databaseService.queryRunner.run = originalQueryRunner;
+
+                done();
+            }.bind(this));
+        });
+    });
+});
