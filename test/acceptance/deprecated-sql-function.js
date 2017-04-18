0b00703b8af29bad437a49e474d4fbd34b8f0051
Raul Ochoa <rochoaf@gmail.com>
2017-04-18 11:13:34 +0200

    Group tests

--- test/acceptance/deprecated-sql-function.js
+++ test/acceptance/deprecated-sql-function.js
@@ -4,38 +4,6 @@ var assert = require('assert');
 var testHelper = require('../helper');
 
 describe('deprecated-sql-function analysis', function () {
-
-    before(function(done) {
-        testHelper.executeQuery([
-            'CREATE OR REPLACE FUNCTION DEP_EXT_test_deprecated_fn(',
-            '    query text, columns text[], a numeric, b numeric, c numeric, d text, table_name text, operation text',
-            ')',
-            'RETURNS VOID AS $$',
-            '    BEGIN',
-            '        IF operation = \'create\' THEN',
-            '            EXECUTE \'CREATE TABLE \' || table_name || \' ',
-            '                (cartodb_id numeric, the_geom geometry, a int, b int, c int)\';',
-            '        ELSIF operation = \'populate\' THEN',
-            '            EXECUTE \'INSERT INTO \' || table_name || \' ',
-            '                SELECT',
-            '                    cartodb_id,',
-            '                    st_setsrid(st_makepoint(st_x(the_geom) + \' || a || \',',
-            '                    st_y(the_geom) + \' || b + c || \'), 4326) the_geom,',
-            '                    \' || a || \', \' || b || \', \' || c || \'',
-            '                FROM (\' || query || \') _q\';',
-            '        END IF;',
-            '    END;',
-            '$$ LANGUAGE plpgsql;'
-        ].join('\n'), done);
-    });
-
-    after(function(done) {
-        testHelper.executeQuery(
-            'DROP FUNCTION DEP_EXT_test_deprecated_fn(text, text[], numeric, numeric, numeric, text, text, text)',
-            done
-        );
-    });
-
     var QUERY_SOURCE = [
         'WITH sources AS (',
         '   select i as cartodb_id, st_setsrid(st_makepoint(i,i), 4326) as the_geom',
@@ -44,80 +12,114 @@ describe('deprecated-sql-function analysis', function () {
         'select *, st_x(the_geom) as x, st_y(the_geom) as y from sources'
     ].join('\n');
 
-    function deprecatedSqlFnDefinition(fnArgs) {
-        return {
-            type: 'deprecated-sql-function',
-            params: {
-                function_name: 'DEP_EXT_test_deprecated_fn',
-                primary_source: {
-                    type: 'source',
-                    params: {
-                        query: QUERY_SOURCE
-                    }
+    describe('basics', function() {
+        before(function(done) {
+            testHelper.executeQuery([
+                'CREATE OR REPLACE FUNCTION DEP_EXT_test_deprecated_fn(',
+                '    query text, columns text[], a numeric, b numeric, c numeric, d text, table_name text, operation text',
+                ')',
+                'RETURNS VOID AS $$',
+                '    BEGIN',
+                '        IF operation = \'create\' THEN',
+                '            EXECUTE \'CREATE TABLE \' || table_name || \' ',
+                '                (cartodb_id numeric, the_geom geometry, a int, b int, c int)\';',
+                '        ELSIF operation = \'populate\' THEN',
+                '            EXECUTE \'INSERT INTO \' || table_name || \' ',
+                '                SELECT',
+                '                    cartodb_id,',
+                '                    st_setsrid(st_makepoint(st_x(the_geom) + \' || a || \',',
+                '                    st_y(the_geom) + \' || b + c || \'), 4326) the_geom,',
+                '                    \' || a || \', \' || b || \', \' || c || \'',
+                '                FROM (\' || query || \') _q\';',
+                '        END IF;',
+                '    END;',
+                '$$ LANGUAGE plpgsql;'
+            ].join('\n'), done);
+        });
+
+        after(function(done) {
+            testHelper.executeQuery(
+                'DROP FUNCTION DEP_EXT_test_deprecated_fn(text, text[], numeric, numeric, numeric, text, text, text)',
+                done
+            );
+        });
+
+        function deprecatedSqlFnDefinition(fnArgs) {
+            return {
+                type: 'deprecated-sql-function',
+                params: {
+                    function_name: 'DEP_EXT_test_deprecated_fn',
+                    primary_source: {
+                        type: 'source',
+                        params: {
+                            query: QUERY_SOURCE
+                        }
+                    },
+                    function_args: fnArgs || [1, 2, 3, 'wadus'],
+                }
+            };
+        }
+
+        function expectedRows(a, b, c) {
+            return [
+                {
+                    cartodb_id: 1,
+                    a: a,
+                    b: b,
+                    c: c,
+                    st_x: 1 + a,
+                    st_y: 1 + b + c
                 },
-                function_args: fnArgs || [1, 2, 3, 'wadus'],
-            }
-        };
-    }
+                {
+                    cartodb_id: 2,
+                    a: a,
+                    b: b,
+                    c: c,
+                    st_x: 2 + a,
+                    st_y: 2 + b + c
+                },
+                {
+                    cartodb_id: 3,
+                    a: a,
+                    b: b,
+                    c: c,
+                    st_x: 3 + a,
+                    st_y: 3 + b + c
+                },
+            ];
+        }
 
-    function expectedRows(a, b, c) {
-        return [
-            {
-                cartodb_id: 1,
-                a: a,
-                b: b,
-                c: c,
-                st_x: 1 + a,
-                st_y: 1 + b + c
-            },
-            {
-                cartodb_id: 2,
-                a: a,
-                b: b,
-                c: c,
-                st_x: 2 + a,
-                st_y: 2 + b + c
-            },
-            {
-                cartodb_id: 3,
-                a: a,
-                b: b,
-                c: c,
-                st_x: 3 + a,
-                st_y: 3 + b + c
-            },
-        ];
-    }
-
-    it('happy case', function (done) {
-        testHelper.createAnalyses(deprecatedSqlFnDefinition(), function(err, result) {
-            assert.ifError(err);
-            var query = 'select cartodb_id, a, b, c, st_x(the_geom), st_y(the_geom) from (' +
-                result.getRoot().getQuery() +
-                ') _q';
-            testHelper.getRows(query, function(err, rows) {
+        it('happy case', function (done) {
+            testHelper.createAnalyses(deprecatedSqlFnDefinition(), function(err, result) {
                 assert.ifError(err);
-                assert.equal(rows.length, 3);
-                assert.deepEqual(rows, expectedRows(1, 2, 3));
-                return done();
+                var query = 'select cartodb_id, a, b, c, st_x(the_geom), st_y(the_geom) from (' +
+                    result.getRoot().getQuery() +
+                    ') _q';
+                testHelper.getRows(query, function(err, rows) {
+                    assert.ifError(err);
+                    assert.equal(rows.length, 3);
+                    assert.deepEqual(rows, expectedRows(1, 2, 3));
+                    return done();
+                });
             });
         });
-    });
 
-    it('change extra args', function (done) {
-        var extraArgs = [9, 8, 7, 'wadus'];
-        testHelper.createAnalyses(deprecatedSqlFnDefinition(extraArgs), function(err, result) {
-            assert.ifError(err);
-            var query = 'select cartodb_id, a, b, c, st_x(the_geom), st_y(the_geom) from (' +
-                result.getRoot().getQuery() +
-                ') _q';
-            testHelper.getRows(query, function(err, rows) {
+        it('change extra args', function (done) {
+            var extraArgs = [9, 8, 7, 'wadus'];
+            testHelper.createAnalyses(deprecatedSqlFnDefinition(extraArgs), function(err, result) {
                 assert.ifError(err);
-                assert.equal(rows.length, 3);
-                assert.deepEqual(rows, expectedRows.apply(null, extraArgs));
-                return done();
+                var query = 'select cartodb_id, a, b, c, st_x(the_geom), st_y(the_geom) from (' +
+                    result.getRoot().getQuery() +
+                    ') _q';
+                testHelper.getRows(query, function(err, rows) {
+                    assert.ifError(err);
+                    assert.equal(rows.length, 3);
+                    assert.deepEqual(rows, expectedRows.apply(null, extraArgs));
+                    return done();
+                });
             });
         });
+
     });
 
     describe('invalid schema', function() {
