259afbbe13eb3b9b1d51127ab54cce51995091ed
Raul Ochoa <rochoaf@gmail.com>
2017-04-18 13:14:57 +0200

    Add another example, with two input nodes, based on an existing PR

--- docs/deprecated-sql-function.md
+++ docs/deprecated-sql-function.md
@@ -93,6 +93,48 @@ select * from DEP_EXT_buffer(
 );
 ```
 
-### Concave hull
+### Spatial Interpolation as in https://github.com/CartoDB/camshaft/pull/153
 
-TBA.
+```sql
+CREATE OR REPLACE FUNCTION DEP_EXT_SpatialInterpolation(
+        source_query text, source_columns text[],
+        target_query text, target_columns text[],
+        val_column text,
+        method numeric, -- 0=nearest neighbor, 1=barymetric, 2=IDW
+        number_of_neighbors numeric, -- DEFAULT 0, -- 0=unlimited
+        decay_order numeric, -- DEFAULT 0,
+        table_name text,
+        operation text
+    )
+    RETURNS VOID AS $$
+        BEGIN
+            IF operation = 'create' THEN
+                EXECUTE 'CREATE TABLE ' || table_name || ' AS ' ||
+                    -- The table will have the schema of target query but with an extra column from the source query.
+                    'SELECT _target.*, _source.' || val_column ||
+                    ' FROM (' || target_query || ') _target, (' || source_query || ') _source LIMIT 0';
+            ELSIF operation = 'populate' THEN
+                EXECUTE 'INSERT INTO ' || table_name || ' ' ||
+                    'WITH ' ||
+                    '_target AS (' || target_query || '),' ||
+                    '_source AS (' ||
+                    '  SELECT array_agg(the_geom) as _geo,' ||
+                    '  array_agg( ' || val_column || '::numeric ) as _values' ||
+                    '  FROM (' || source_query || ') _s' ||
+                    '  WHERE ' || val_column || ' is not null' ||
+                    ')' ||
+                    'SELECT _target.*,' ||
+                    '  cdb_crankshaft.CDB_MockSpatialInterpolation(' ||
+                    '    _source._geo,' ||
+                    '    _source._values,' ||
+                    '    CASE WHEN GeometryType(_target.the_geom) = ''POINT'' THEN _target.the_geom ELSE ST_Centroid(_target.the_geom) END,' ||
+                    '    ' || method || ','
+                    '    ' || number_of_neighbors || ','
+                    '    ' || decay_order || ''
+                    '  ) AS ' || val_column || ' '
+                    'FROM _target, _source';
+            END IF;
+        END;
+$$ LANGUAGE plpgsql;
+
+```
--- test/acceptance/deprecated-sql-function-examples.js
+++ test/acceptance/deprecated-sql-function-examples.js
@@ -78,4 +78,81 @@ describe('deprecated-sql-function examples', function () {
 
     });
 
+    describe('Spatial Interpolation', function() {
+        before(function(done) {
+            readSql('DEP_EXT_SpatialInterpolation.sql', function(err, content) {
+                if (err) {
+                    return done(err);
+                }
+                testHelper.executeQuery(content, done);
+            });
+        });
+
+        after(function(done) {
+            /*
+            primary_source_query text, primary_source_columns text[],
+            secondary_source_query text, secondary_source_columns text[],
+            val_column text,
+            method numeric, -- 0=nearest neighbor, 1=barymetric, 2=IDW
+            number_of_neighbors numeric DEFAULT 0, -- 0=unlimited
+            decay_order numeric DEFAULT 0,
+            table_name text, operation text
+            */
+            testHelper.executeQuery(
+                'DROP FUNCTION DEP_EXT_SpatialInterpolation(text, text[], text, text[], text, numeric, numeric, numeric, text, text)',
+                done
+            );
+        });
+
+        var QUERY_SOURCE = [
+            'WITH sources AS (',
+            '   select i as cartodb_id, st_setsrid(st_makepoint(0, i), 4326) as the_geom, i * 100 * random() as wadus',
+            '   from generate_series(1,3) as i',
+            ')',
+            'select *, st_x(the_geom) as x, st_y(the_geom) as y from sources'
+        ].join('\n');
+
+        var QUERY_TARGET = [
+            'WITH targets AS (',
+            '   select i as cartodb_id, st_setsrid(st_makepoint(0, i), 4326) as the_geom',
+            '   from generate_series(1,3) as i',
+            ')',
+            'select *, st_x(the_geom) as x, st_y(the_geom) as y from targets'
+        ].join('\n');
+
+        function spatialInterpolationDeprecatedSqlFnDefinition() {
+            return {
+                type: 'deprecated-sql-function',
+                params: {
+                    function_name: 'DEP_EXT_SpatialInterpolation',
+                    primary_source: {
+                        type: 'source',
+                        params: {
+                            query: QUERY_SOURCE
+                        }
+                    },
+                    secondary_source: {
+                        type: 'source',
+                        params: {
+                            query: QUERY_TARGET
+                        }
+                    },
+                    function_args: ['wadus', 1, 5, 0],
+                }
+            };
+        }
+
+        it('should create a polygon with the expected area', function (done) {
+            testHelper.getResult(spatialInterpolationDeprecatedSqlFnDefinition(), function(err, rows) {
+                assert.ok(!err, err);
+                assert.equal(rows.length, 3);
+                rows.forEach(function(row) {
+                    assert.ok(row.hasOwnProperty('wadus'));
+                });
+                return done();
+            });
+        });
+
+    });
+
 });
