c13d3e19771f658a7e35a0fd88447d27fde78482
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-05-17 19:28:50 +0200

    First attempt of analysis for new OBS multiple measures API

--- test/acceptance/data-observatory-multiple-measures.js
+++ test/acceptance/data-observatory-multiple-measures.js
@@ -4,7 +4,7 @@ var assert = require('assert');
 var testHelper = require('../helper');
 
 describe('data-observatory-multiple-measures analysis', function() {
-    function doMultipleMeasuresDefinition(numerators, columnNames) {
+    function doMultipleMeasuresDefinition(params) {
         return {
             type: 'data-observatory-multiple-measures',
             params: {
@@ -14,17 +14,30 @@ describe('data-observatory-multiple-measures analysis', function() {
                         query: 'select * from atm_machines limit 2'
                     }
                 },
-                numerators: numerators,
-                column_names: columnNames
+                numerators: params.numerators,
+                normalizations: params.normalizations,
+                denominators: params.denominators,
+                geom_ids: params.geom_ids,
+                numerator_timespans: params.numerator_timespans,
+                column_names: params.columnNames
             }
         };
     }
 
     var numerators = ['es.ine.t2_2', 'es.ine.t2_1'];
+    var normalizations = ['denominated', 'denominated'];
+    var denominators = ['es.ine.t1_1', 'es.ine.t1_1'];
+    var geom_ids = ['es.ine.the_geom', 'es.ine.the_geom'];
+    var numerator_timespans = ['2011', '2011'];
     var columnNames = ['females', 'males'];
 
     it('should fail on different number of numerators and column names', function (done) {
-        var def = doMultipleMeasuresDefinition(numerators, []);
+        var def = doMultipleMeasuresDefinition({
+            numerators: numerators,
+            normalizations: normalizations,
+            columnNames: []
+        });
+
         testHelper.getResult(def, function(err) {
             assert.ok(err);
             assert.equal(err.message, 'The number of numerators=2 does not match the number of column_names=0');
@@ -32,8 +45,27 @@ describe('data-observatory-multiple-measures analysis', function() {
         });
     });
 
+    it('should fail on empty normalizations', function (done) {
+        var def = doMultipleMeasuresDefinition({
+            numerators: numerators,
+            normalizations: [],
+            columnNames: columnNames
+        });
+
+        testHelper.getResult(def, function(err) {
+            assert.ok(err);
+            assert.equal(err.message, 'The normalizations array cannot be empty');
+            return done();
+        });
+    });
+
     it('should fail on empty numerators', function (done) {
-        var def = doMultipleMeasuresDefinition([], []);
+        var def = doMultipleMeasuresDefinition({
+            numerators: [],
+            normalizations: normalizations,
+            columnNames: columnNames
+        });
+
         testHelper.getResult(def, function(err) {
             assert.ok(err);
             assert.equal(err.message, 'The numerators array cannot be empty');
@@ -41,8 +73,34 @@ describe('data-observatory-multiple-measures analysis', function() {
         });
     });
 
-    it('should create an analysis', function (done) {
-        var def = doMultipleMeasuresDefinition(numerators, columnNames);
+    it('should create an analysis with just mandatory params', function (done) {
+        var def = doMultipleMeasuresDefinition({
+            numerators: numerators,
+            normalizations: normalizations,
+            columnNames: columnNames
+        });
+
+        testHelper.getResult(def, function(err, rows) {
+            assert.ifError(err);
+            rows.forEach(function(row) {
+                columnNames.forEach(function(columnName) {
+                    assert.ok(row.hasOwnProperty(columnName), 'Missing ' + columnName + ' column');
+                });
+            });
+            return done();
+        });
+    });
+
+    it('should create an analysis with all params', function (done) {
+        var def = doMultipleMeasuresDefinition({
+            numerators: numerators,
+            denominators: denominators,
+            normalizations: normalizations,
+            geom_ids: geom_ids,
+            numerator_timespans: numerator_timespans,
+            columnNames: columnNames
+        });
+
         testHelper.getResult(def, function(err, rows) {
             assert.ifError(err);
             rows.forEach(function(row) {
