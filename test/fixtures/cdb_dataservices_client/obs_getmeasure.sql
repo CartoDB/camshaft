276a92f95b7b7e55b7ac44311b7943645580c8a4
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-05-23 11:19:17 +0200

    Use exception-safe wrappers for DO get measure

--- test/fixtures/cdb_dataservices_client/obs_getmeasure.sql
+++ test/fixtures/cdb_dataservices_client/obs_getmeasure.sql
@@ -28,3 +28,34 @@ BEGIN
   RETURN;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION cdb_dataservices_client._OBS_GetMeta_exception_safe(
+  geom_ref Geometry(Geometry, 4326),
+  params json,
+  max_timespan_rank integer DEFAULT NULL,
+  max_score_rank integer DEFAULT NULL,
+  target_geoms integer DEFAULT NULL
+)
+RETURNS json
+AS $$
+BEGIN
+  RETURN '{}'::json;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION cdb_dataservices_client._OBS_GetData_exception_safe(
+  geomvals geomval[],
+  params json,
+  merge boolean DEFAULT true
+)
+RETURNS TABLE(
+  id int,
+  data json)
+AS $$
+BEGIN
+  RETURN QUERY EXECUTE $query$
+    SELECT 1, '{}'::JSON
+  $query$;
+  RETURN;
+END;
+$$ LANGUAGE plpgsql;
