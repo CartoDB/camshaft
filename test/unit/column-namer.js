c30b8abb61c91b3ab555ea7168efb9741d00ad6d
Javier Goizueta <jgoizueta@gmail.com>
2017-09-21 13:15:15 +0200

    Remove use of params.id suffixes to make unique column names
    Since params.id is not hashed into the node's identifier that lead to having different queries with the same id (ie. cached together)

--- test/unit/column-namer.js
+++ test/unit/column-namer.js
@@ -7,38 +7,23 @@ var ColumnNamer = require('../../lib/node/column-namer');
 describe('column-namer', function() {
 
     it('should not change unique names', function() {
-        var namer = new ColumnNamer({ id: 'xxx' }, ['col1', 'col2', 'col3' ]);
+        var namer = new ColumnNamer(['col1', 'col2', 'col3' ]);
         assert.equal(namer.uniqueName('col4'), 'col4');
     });
 
-    it('should use the node id if available', function() {
-        var namer = new ColumnNamer({ id: 'a0' }, ['col1', 'col2', 'col3' ]);
-        assert.equal(namer.uniqueName('col2'), 'col2_a0');
-    });
-
-    it('should use a count when no id', function() {
-        var namer = new ColumnNamer({}, ['col1', 'col2', 'col3' ]);
+    it('should use a count suffix', function() {
+        var namer = new ColumnNamer(['col1', 'col2', 'col3' ]);
         assert.equal(namer.uniqueName('col2'), 'col2_2');
     });
 
-    it('should add a counter to the node id if necessary', function() {
-        var namer = new ColumnNamer({ id: 'a0' }, ['col1', 'col2', 'col3', 'col2_a0' ]);
-        assert.equal(namer.uniqueName('col2'), 'col2_a0_2');
-    });
-
-    it('should add higher counts to the node id if necessary', function() {
-        var namer = new ColumnNamer({ id: 'a0' }, ['col1', 'col2', 'col3', 'col2_a0', 'col2_a0_2', 'col2_a0_3' ]);
-        assert.equal(namer.uniqueName('col2'), 'col2_a0_4');
-    });
-
     it('should use higher counts if necessary', function() {
-        var namer = new ColumnNamer({}, ['col1', 'col2', 'col3', 'col2_2', 'col2_3', 'col2_4' ]);
+        var namer = new ColumnNamer(['col1', 'col2', 'col3', 'col2_2', 'col2_3', 'col2_4' ]);
         assert.equal(namer.uniqueName('col2'), 'col2_5');
     });
 
     it('should accept table names in the columns', function() {
-        var namer = new ColumnNamer({ id: 'xxx' }, ['table.col1', 'table.col2', 'table.col3' ]);
-        assert.equal(namer.uniqueName('col1'), 'col1_xxx');
+        var namer = new ColumnNamer(['table.col1', 'table.col2', 'table.col3' ]);
+        assert.equal(namer.uniqueName('col1'), 'col1_2');
         assert.equal(namer.uniqueName('col4'), 'col4');
     });
 
