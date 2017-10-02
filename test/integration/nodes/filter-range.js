e9865b9f84635f615970721d713129b10011f641
Carlos Matallín <matallo@carto.com>
2017-10-02 17:38:00 +0200

    filter: range, or equal

--- test/integration/nodes/filter-range.js
+++ test/integration/nodes/filter-range.js
@@ -10,10 +10,32 @@ describe('filter-range', function() {
     var column = 'wadus_column';
     var source = new Source(owner, {query: 'select * from table'});
 
+    it('should work with minOrEqual param', function() {
+        var filterRange = new FilterRange(owner, { source: source, column: column, minOrEqual: 100 });
+
+        assert.equal(filterRange.column, column);
+        assert.equal(filterRange.minOrEqual, 100);
+        assert.equal(filterRange.maxOrEqual, null);
+        assert.equal(filterRange.min, null);
+        assert.equal(filterRange.max, null);
+    });
+
+    it('should work with maxOrEqual param', function() {
+        var filterRange = new FilterRange(owner, { source: source, column: column, maxOrEqual: 100 });
+
+        assert.equal(filterRange.column, column);
+        assert.equal(filterRange.minOrEqual, null);
+        assert.equal(filterRange.maxOrEqual, 100);
+        assert.equal(filterRange.min, null);
+        assert.equal(filterRange.max, null);
+    });
+
     it('should work with min param', function() {
         var filterRange = new FilterRange(owner, { source: source, column: column, min: 100 });
 
         assert.equal(filterRange.column, column);
+        assert.equal(filterRange.minOrEqual, null);
+        assert.equal(filterRange.maxOrEqual, null);
         assert.equal(filterRange.min, 100);
         assert.equal(filterRange.max, null);
     });
@@ -22,11 +44,13 @@ describe('filter-range', function() {
         var filterRange = new FilterRange(owner, { source: source, column: column, max: 10000 });
 
         assert.equal(filterRange.column, column);
+        assert.equal(filterRange.minOrEqual, null);
+        assert.equal(filterRange.maxOrEqual, null);
         assert.equal(filterRange.min, null);
         assert.equal(filterRange.max, 10000);
     });
 
-    it('should fail when min and max are not provided', function() {
+    it('should fail when minOrEqual, maxOrEqual, min, and max are not provided', function() {
         var filterRange;
 
         assert.throws(
@@ -36,7 +60,7 @@ describe('filter-range', function() {
             function(err) {
                 assert.equal(
                     err.message,
-                    'Range filter expect to have at least one value in min or max numeric params'
+                    'Range filter expect to have at least one value in minOrEqual, maxOrEqual, min, or max numeric params'
                 );
                 return true;
             }
