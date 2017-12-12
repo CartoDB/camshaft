fcc81afb67e8f478e32056fb9be7d5039db42151
Raul Ochoa <rochoaf@gmail.com>
2017-12-12 11:32:53 +0000

    Avoid `set-returning functions are not allowed in CASE` in PostgreSQL 10

--- test/fixtures/cdb_querytables_updated_at.sql
+++ test/fixtures/cdb_querytables_updated_at.sql
@@ -1,15 +1,21 @@
 CREATE OR REPLACE FUNCTION CDB_QueryTables_Updated_At(query text)
     RETURNS TABLE(dbname text, schema_name text, table_name text, updated_at timestamptz)
 AS $$
+    WITH query_tables AS (
+        SELECT
+            CASE WHEN position('atm_machines' in query) > 0 THEN ARRAY['atm_machines']::text[]
+                 WHEN position('multiple_tables' in query) > 0 THEN ARRAY['table_a', 'table_b']::text[]
+                 ELSE ARRAY['fixture_table_name']::text[]
+            END
+        AS table_names
+    )
     SELECT
-    'analysis_api_test_db'::text,
-    'public'::text,
-    CASE WHEN position('atm_machines' in query) > 0 THEN 'atm_machines'::text
-         WHEN position('multiple_tables' in query) > 0 THEN unnest(ARRAY['table_a', 'table_b']::text[])
-         ELSE 'fixture_table_name'::text
-    END,
-    CASE WHEN position('nulltime' in query) > 0
-        THEN null::timestamptz
-        ELSE '2016-07-01 11:40:05.699712+00'::timestamptz
-    END
+        'analysis_api_test_db'::text,
+        'public'::text,
+        unnest(table_names),
+        CASE WHEN position('nulltime' in query) > 0
+            THEN null::timestamptz
+            ELSE '2016-07-01 11:40:05.699712+00'::timestamptz
+        END
+    FROM query_tables
 $$ LANGUAGE SQL;
