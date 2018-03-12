bbef79b80316d9cf3422d37cadede83c47f06a72
Raul Marin <git@rmr.ninja>
2018-03-12 15:42:00 +0100

    TradeArea: Do not reuse the source cartodb_id

--- lib/node/nodes/sql/trade-area.sql
+++ lib/node/nodes/sql/trade-area.sql
@@ -9,13 +9,16 @@ _cdb_analysis_isochrones AS (
       '{{=it.kind}}',
       ARRAY[{{=it.isolines}}]::integer[]
     ) as isochrone,
+    _cdb_analysis_source_points.cartodb_id as source_cartodb_id,
     {{=it.columnsQuery}}
   FROM _cdb_analysis_source_points
 )
 SELECT
+  row_number() over() as cartodb_id,
   (isochrone).center,
   (isochrone).data_range,
   (isochrone).the_geom,
+  source_cartodb_id,
   {{=it.columnsQuery}}
 FROM _cdb_analysis_isochrones
 ORDER BY (isochrone).data_range DESC
